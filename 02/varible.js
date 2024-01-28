7ec8841c77d8c2dd3e481ddcfba4223c7f444b30
Tejasbisen786 <bisentejas@gmail.com>
2024-01-28 23:38:18 +0530

    02 video Completed

---
+++ 02/varible.js
@@ -0,0 +1,65 @@
+// variables
+// ***VAR***
+//  *** VAR | LET | CONST 
+// DataType varname = Strogly Typed language
+// Js -> loose 
+// var age=21
+// var num1=10
+// var num2=20
+// console.log(num1+num2);
+// age="tejas"
+// console.log(age);
+
+//Naming convension
+// pref cammel case 
+// ** firstName (pref)
+//firstname
+// first_name
+// FirstName
+// firstName
+//wrong name
+// spaces not allowed
+// - used for separate the worlds
+// #age
+// 1age
+
+// **** Var Scope : Global 
+//  **** pref ( alwyas used let )
+// if(true){
+//     var age=23
+// }
+// console.log(age);
+
+// console.log(age);
+// var age=20  // undefined
+// console.log(age);
+
+// {
+    // var a=10s
+// }
+// console.log(a);
+
+// *****LET******
+
+let age=22
+// console.log(age);
+if(true){
+   let x=34
+//    console.log(x);
+// console.log(`my age is :  ${age}`);
+}
+
+// console.log(x);
+
+
+// *******CONST***********
+// scope : Local Scope s
+// Value cant be change once you assigned it 
+
+const myName= "Tejas"
+// myName="Tushr"
+// conssole.log(myName);
+
+const ages=67
+// ages=56
+// console.log(ages + 67);
