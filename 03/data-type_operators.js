dac7435516d70543f14082eade7b51f6e2b03f70
Tejasbisen786 <bisentejas@gmail.com>
2024-01-29 20:56:06 +0530

    03 Video Completed

---
+++ 03/data-type_operators.js
@@ -0,0 +1,52 @@
+// Data Types
+
+// Numbers
+let num=3.25
+let num2=12
+// console.log(num+num2); //adding Numbers
+// console.log( num * num2); // multiplying Numbers
+
+// Strings
+
+const firstName= "Tejas"
+const LastName= "Bisen"
+const collegeName= "HVPM COET"
+// console.log( +  " " + collegeName);
+firstName
+console.log(firstName + LastName);
+
+// Booleans - True / False
+
+let isLoggedin= false
+console.log(isLoggedin);
+
+// Null
+
+let lastLoginName= null
+// SPace use
+console.log(lastLoginName);
+
+// undefiend
+
+let lastLoginName2= undefined
+// not taking space in memory
+console.log(lastLoginName2);
+
+// objects
+
+const person={
+    firstName: "Tejas",
+    lastName:"bisen",
+    age:21,
+    isLoggedin : false,
+    lastLoginDate: null, // pref( used comma to separate)
+}
+console.log(person);
+console.log( person.age);
+console.log(person.lastLoginDate);
+
+
+
+
+
+
---
+++ 03/opearators.js
@@ -0,0 +1,46 @@
+
+const StartName= "Tejas"
+const lastName="Bisen"
+
+console.log(StartName + lastName);
+
+let num1= 10
+let num2=70
+console.log(num1*num2);
+
+
+if(0){
+console.log(true);
+}
+
+// 1-> true
+// 0 -> false 
+
+//js consider mapping value while calculating
+
+// true-> 1
+// false-> 0
+
+let isLoggedin= false
+
+console.log(isLoggedin + 10);
+
+// Strings '1' + '1'
+
+console.log('1'+ '1');
+console.log('1'+ 1);
+console.log(1+ '1');
+console.log('1'* 11);
+console.log("2334" * 1);  // convert into the numbers
+console.log( typeof ('a'*1));  // NaN
+
+// TypeOf Opearator
+// to find the type of a number 
+console.log( typeof(213));
+
+
+
+
+
+
+
